50a18e9c5f5dbc1655c25b61ccff92a28ca4578f
elim <yeelimzhang@163.com>
2015-12-11 17:00:38 +0800

    去掉无用的引入信息

--- learn/src/main/java/com/yeelim/learn/ehcache/listener/MyHttpServletRequest.java
+++ learn/src/main/java/com/yeelim/learn/ehcache/listener/MyHttpServletRequest.java
@@ -24,6 +24,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpUpgradeHandler;
 import javax.servlet.http.Part;
 
 /**
@@ -629,6 +630,24 @@ public class MyHttpServletRequest implements HttpServletRequest {
 	public void logout() throws ServletException {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public long getContentLengthLong() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String changeSessionId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T extends HttpUpgradeHandler> T upgrade(Class<T> arg0) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
